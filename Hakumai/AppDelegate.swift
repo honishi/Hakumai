4657dfe5a1234415543872e5bb2e4dacebb8a662
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-11-09 08:47:11 +0900

    Initial Commit

---
+++ Hakumai/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Hakumai
+//
+//  Created by Hiroyuki Onishi on 11/9/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Hakumai/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Hakumai
+//
+//  Created by Hiroyuki Onishi on 11/9/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
