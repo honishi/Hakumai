1d2069c58f98fea6cdbd2966ef424bc480eb9b0d
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2014-12-15 13:51:44 +0900

    added esc key to close preference window

---
+++ Hakumai/ViewController/Preference/PreferenceWindow.swift
@@ -0,0 +1,24 @@
+//
+//  PreferenceWindow.swift
+//  Hakumai
+//
+//  Created by Hiroyuki Onishi on 12/15/14.
+//  Copyright (c) 2014 Hiroyuki Onishi. All rights reserved.
+//
+
+import Foundation
+import AppKit
+
+class PreferenceWindow: NSWindow {
+    // MARK: - NSObject Overrides
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        self.alwaysOnTop = true
+    }
+
+    // MARK: - NSResponder Overrides
+    // http://genjiapp.com/blog/2012/10/25/how-to-develop-a-preferences-window-for-os-x-app.html
+    override func cancelOperation(sender: AnyObject?) {
+        self.close()
+    }
+}
