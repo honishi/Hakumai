e5aaba67a0fc348828ba665d7c9feb985ac87e57
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2015-11-14 06:33:16 +0900

    remove all submodules

--- Hakumai/Hakumai-Bridging-Header.h
+++ Hakumai/Hakumai-Bridging-Header.h
@@ -2,8 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <sqlite3.h>
-#import "FMDB.h"
-#import "SSKeychain.h"
+// #import <sqlite3.h>
 #import <CommonCrypto/CommonCrypto.h>
-#import "Ono.h"
