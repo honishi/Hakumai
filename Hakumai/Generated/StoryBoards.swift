b35969a8fb1f4956d8784d6a0f065e0fa2eb37d4
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2018-12-16 01:57:16 +0900

    add swiftgen

---
+++ Hakumai/Generated/StoryBoards.swift
@@ -0,0 +1,82 @@
+// swiftlint:disable all
+// Generated using SwiftGen, by O.Halligon — https://github.com/SwiftGen/SwiftGen
+
+// swiftlint:disable sorted_imports
+import Foundation
+import AppKit
+
+// swiftlint:disable superfluous_disable_command
+// swiftlint:disable file_length
+
+// MARK: - Storyboard Scenes
+
+// swiftlint:disable explicit_type_interface identifier_name line_length type_body_length type_name
+internal enum StoryboardScene {
+  internal enum MainWindowController: StoryboardType {
+    internal static let storyboardName = "MainWindowController"
+
+    internal static let initialScene = InitialSceneType<Hakumai.MainWindowController>(storyboard: MainWindowController.self)
+
+    internal static let handleNameAddViewController = SceneType<Hakumai.HandleNameAddViewController>(storyboard: MainWindowController.self, identifier: "HandleNameAddViewController")
+  }
+  internal enum PreferenceWindowController: StoryboardType {
+    internal static let storyboardName = "PreferenceWindowController"
+
+    internal static let initialScene = InitialSceneType<Hakumai.PreferenceWindowController>(storyboard: PreferenceWindowController.self)
+
+    internal static let generalViewController = SceneType<Hakumai.GeneralViewController>(storyboard: PreferenceWindowController.self, identifier: "GeneralViewController")
+
+    internal static let muteAddViewController = SceneType<Hakumai.MuteAddViewController>(storyboard: PreferenceWindowController.self, identifier: "MuteAddViewController")
+
+    internal static let muteViewController = SceneType<Hakumai.MuteViewController>(storyboard: PreferenceWindowController.self, identifier: "MuteViewController")
+
+    internal static let preferenceWindowController = SceneType<Hakumai.PreferenceWindowController>(storyboard: PreferenceWindowController.self, identifier: "PreferenceWindowController")
+  }
+  internal enum UserWindowController: StoryboardType {
+    internal static let storyboardName = "UserWindowController"
+
+    internal static let initialScene = InitialSceneType<Hakumai.UserWindowController>(storyboard: UserWindowController.self)
+
+    internal static let userWindowController = SceneType<Hakumai.UserWindowController>(storyboard: UserWindowController.self, identifier: "UserWindowController")
+  }
+}
+// swiftlint:enable explicit_type_interface identifier_name line_length type_body_length type_name
+
+// MARK: - Implementation Details
+
+internal protocol StoryboardType {
+  static var storyboardName: String { get }
+}
+
+internal extension StoryboardType {
+  static var storyboard: NSStoryboard {
+    let name = NSStoryboard.Name(self.storyboardName)
+    return NSStoryboard(name: name, bundle: Bundle(for: BundleToken.self))
+  }
+}
+
+internal struct SceneType<T> {
+  internal let storyboard: StoryboardType.Type
+  internal let identifier: String
+
+  internal func instantiate() -> T {
+    let identifier = NSStoryboard.SceneIdentifier(self.identifier)
+    guard let controller = storyboard.storyboard.instantiateController(withIdentifier: identifier) as? T else {
+      fatalError("Controller '\(identifier)' is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+internal struct InitialSceneType<T> {
+  internal let storyboard: StoryboardType.Type
+
+  internal func instantiate() -> T {
+    guard let controller = storyboard.storyboard.instantiateInitialController() as? T else {
+      fatalError("Controller is not of the expected class \(T.self).")
+    }
+    return controller
+  }
+}
+
+private final class BundleToken {}
--- Podfile
+++ Podfile
@@ -4,12 +4,14 @@ inhibit_all_warnings!
 target 'Hakumai' do
   use_frameworks!
 
-  pod 'SwiftLint', '0.29.1'
+  pod 'SwiftLint', '~> 0.29.1'
+  pod 'SwiftGen', '~> 6.0.2'
+  pod 'XCGLogger', '~> 6.1.0'
+
+  pod 'Sparkle', '~> 1.14.0'
   pod 'FMDB', '~> 2.6.2'
   pod 'Ono', '~> 1.2.2'
-  pod 'Sparkle', '~> 1.14.0'
   pod 'SAMKeychain', '~> 1.5.0'
-  pod 'XCGLogger', '~> 6.1.0'
 
   target 'HakumaiTests' do
     inherit! :search_paths
