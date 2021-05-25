9e4473086d8c26eeaec69fa9a7ea7316dc2f7a95
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2021-05-26 00:42:00 +0900

    add test case for user icon resolving

--- HakumaiTests/Models/NicoUtilityTests.swift
+++ HakumaiTests/Models/NicoUtilityTests.swift
@@ -30,11 +30,31 @@ final class NicoUtilityTests: XCTestCase {
 
     // MARK: - User Account
     func testUserIcon() {
-        var expected = ""
+        var expected: String? = ""
         var actual: String? = ""
+
+        expected = nil
+        actual = NicoUtility.shared.userIconUrl(for: "XXX")?.absoluteString
+        XCTAssert(actual == expected)
+
+        expected = "https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/0/2.jpg"
+        actual = NicoUtility.shared.userIconUrl(for: "2")?.absoluteString
+        XCTAssert(actual == expected)
+
+        expected = "https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/0/9005.jpg"
+        actual = NicoUtility.shared.userIconUrl(for: "9005")?.absoluteString
+        XCTAssert(actual == expected)
 
         expected = "https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/9/99998.jpg"
         actual = NicoUtility.shared.userIconUrl(for: "99998")?.absoluteString
+        XCTAssert(actual == expected)
+
+        expected = "https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/1/12346.jpg"
+        actual = NicoUtility.shared.userIconUrl(for: "12346")?.absoluteString
+        XCTAssert(actual == expected)
+
+        expected = "https://secure-dcdn.cdn.nimg.jp/nicoaccount/usericon/25/252346.jpg"
+        actual = NicoUtility.shared.userIconUrl(for: "252346")?.absoluteString
         XCTAssert(actual == expected)
     }
 
