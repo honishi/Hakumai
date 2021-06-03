2dacca3b6cf2dc14e4e639e88c68d405b0ed5d72
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2021-06-03 18:57:17 +0900

    localize comment menu items

--- Hakumai/Generated/Localize.swift
+++ Hakumai/Generated/Localize.swift
@@ -12,6 +12,10 @@ import Foundation
 internal enum L10n {
   /// Active Users
   internal static let activeUser = L10n.tr("Localizable", "active_user")
+  /// Add Handle Name
+  internal static let addHandleName = L10n.tr("Localizable", "add_handle_name")
+  /// Add to Mute User
+  internal static let addToMuteUser = L10n.tr("Localizable", "add_to_mute_user")
   /// Comments
   internal static let commentCount = L10n.tr("Localizable", "comment_count")
   /// ⌘N (Empty ⏎ to scroll to bottom)
@@ -20,12 +24,22 @@ internal enum L10n {
   internal static let communityId = L10n.tr("Localizable", "community_id")
   /// Community Name
   internal static let communityName = L10n.tr("Localizable", "community_name")
+  /// Copy Commnet
+  internal static let copyComment = L10n.tr("Localizable", "copy_comment")
   /// Elapsed Time
   internal static let elapsedTime = L10n.tr("Localizable", "elapsed_time")
   /// Live Title
   internal static let liveTitle = L10n.tr("Localizable", "live_title")
   /// ⌘L (Live URL, Live#)
   internal static let liveUrlTextFieldPlaceholder = L10n.tr("Localizable", "live_url_text_field_placeholder")
+  /// Open URL in Comment
+  internal static let openUrlInComment = L10n.tr("Localizable", "open_url_in_comment")
+  /// Open User Page
+  internal static let openUserPage = L10n.tr("Localizable", "open_user_page")
+  /// Remove Handle Name
+  internal static let removeHandleName = L10n.tr("Localizable", "remove_handle_name")
+  /// Report as NG User
+  internal static let reportAsNgUser = L10n.tr("Localizable", "report_as_ng_user")
   /// Speak
   internal static let speakComment = L10n.tr("Localizable", "speak_comment")
   /// Visitors
