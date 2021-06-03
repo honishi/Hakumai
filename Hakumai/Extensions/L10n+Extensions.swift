69fcca7d502e988227473dc0508b11051359bea5
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2021-06-03 21:10:22 +0900

    add l10n utility extension

---
+++ Hakumai/Extensions/L10n+Extensions.swift
@@ -0,0 +1,32 @@
+//
+//  L10n+Extensions.swift
+//  Hakumai
+//
+//  Created by Hiroyuki Onishi on 2021/06/03.
+//  Copyright © 2021 Hiroyuki Onishi. All rights reserved.
+//
+
+import Foundation
+import Cocoa
+
+private let fatalErrorMessageForGetter = "only set this value"
+
+extension NSMenuItem {
+    @IBInspectable
+    private var localizedKey: String? {
+        get { fatalError(fatalErrorMessageForGetter) }
+        set { title = newValue?.localized ?? "" }
+    }
+}
+
+extension NSTextField {
+    @IBInspectable
+    private var localizedKey: String? {
+        get { fatalError(fatalErrorMessageForGetter) }
+        set { stringValue = newValue?.localized ?? "" }
+    }
+}
+
+extension String {
+    var localized: String? { NSLocalizedString(self, comment: "") }
+}
