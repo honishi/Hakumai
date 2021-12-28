3f523602c9fd3c7d8c402bf36e5e4da54d31cf9d
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2021-12-28 13:30:47 +0900

    update NSColor extension

--- Hakumai/Controllers/UIHelper.swift
+++ Hakumai/Controllers/UIHelper.swift
@@ -9,58 +9,22 @@
 import Foundation
 import AppKit
 
-// based on https://github.com/yeahdongcn/UIColor-Hex-Swift
-extension NSColor {
-    convenience init(rgba: String) {
-        var red: CGFloat = 0.0
-        var green: CGFloat = 0.0
-        var blue: CGFloat = 0.0
-        var alpha: CGFloat = 1.0
-
-        if rgba.hasPrefix("#") {
-            let index   = rgba.index(rgba.startIndex, offsetBy: 1)
-            let hex     = String(rgba[index...])
-            let scanner = Scanner(string: hex)
-            var hexValue: CUnsignedLongLong = 0
-            if scanner.scanHexInt64(&hexValue) {
-                if hex.count == 6 {
-                    red   = CGFloat((hexValue & 0xFF0000) >> 16) / 255.0
-                    green = CGFloat((hexValue & 0x00FF00) >> 8)  / 255.0
-                    blue  = CGFloat(hexValue & 0x0000FF) / 255.0
-                } else if hex.count == 8 {
-                    red   = CGFloat((hexValue & 0xFF000000) >> 24) / 255.0
-                    green = CGFloat((hexValue & 0x00FF0000) >> 16) / 255.0
-                    blue  = CGFloat((hexValue & 0x0000FF00) >> 8)  / 255.0
-                    alpha = CGFloat(hexValue & 0x000000FF)         / 255.0
-                } else {
-                    print("invalid rgb string, length should be 7 or 9", terminator: "")
-                }
-            } else {
-                print("scan hex error")
-            }
-        } else {
-            print("invalid rgb string, missing '#' as prefix", terminator: "")
-        }
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
-    }
-}
-
 final class UIHelper {
     // MARK: - Main Window Colors
     static func systemMessageColorBackground() -> NSColor {
-        return NSColor(rgba: "#808080")
+        return NSColor(hex: "#808080")
     }
 
     static func debugMessageColorBackground() -> NSColor {
-        return NSColor(rgba: "#101010")
+        return NSColor(hex: "#101010")
     }
 
     static func scoreColorGreen() -> NSColor {
-        return NSColor(rgba: "#0EA50B")
+        return NSColor(hex: "#0EA50B")
     }
 
     static func roomColorArena() -> NSColor {
-        return NSColor(rgba: "#3c49ff")
+        return NSColor(hex: "#3c49ff")
     }
 
     // MARK: - Font Attributes
