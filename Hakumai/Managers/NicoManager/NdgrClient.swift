2595a50cca69e26a93ea5139eda5dbc8bb196721
Hiroyuki Onishi <onishi.hiroyuki@gmail.com>
2024-08-03 06:13:18 +0900

    call view endpoint

--- Hakumai/Managers/NicoManager/NdgrClient.swift
+++ Hakumai/Managers/NicoManager/NdgrClient.swift
@@ -7,16 +7,52 @@
 //
 
 import Foundation
+import Alamofire
 
 final class NdgrClient: NdgrClientType {
     // Public Properties
     weak var delegate: NdgrClientDelegate?
+
+    // Private Properties
+    private let session: Session
+
+    init(delegate: NdgrClientDelegate? = nil) {
+        self.delegate = delegate
+        session = {
+            let configuration = URLSessionConfiguration.af.default
+            configuration.headers.add(.userAgent(commonUserAgentValue))
+            return Session(configuration: configuration)
+        }()
+    }
 }
 
 // MARK: - Public Functions
 extension NdgrClient {
     func connect(viewUri: URL) {
-        // TODO
-        delegate?.ndgrClientDidConnect(self)
+        _connect(viewUri: viewUri)
+    }
+}
+
+// MARK: - Private Functions
+private extension NdgrClient {
+    func _connect(viewUri: URL) {
+        session.request(
+            viewUri,
+            method: .get,
+            parameters: ["at": Int(Date().timeIntervalSince1970)]
+        )
+        .validate()
+        .responseData { [weak self] in
+            guard let me = self else { return }
+            switch $0.result {
+            case .success(let data):
+                log.debug(data)
+                // TODO: message parse
+            case .failure(let error):
+                log.error("error in connecting to view: \(error)")
+            }
+            // TODO
+            me.delegate?.ndgrClientDidConnect(me)
+        }
     }
 }
